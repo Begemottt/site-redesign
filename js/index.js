312fe8c617aab2158e449043363ce15e84af92c5
Begemottt <55757703+Begemottt@users.noreply.github.com>
2021-03-28 15:18:13 -0700

    Javascript!!
    Added javascript and changed around how the navigation menus work

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
 import * as _ from 'lodash';
 import '../scss/styles.scss';
 import * as $ from 'jquery';
+
+// jQuery event listener functions go in here
+$("document").ready(function(){
+
+    $( "#navbutton" ).on( "click", function() {
+        $('#verticalnav').toggleClass('show');
+    });
+
+});
+
+window.onscroll =  function() {display_nav()};
+
+function display_nav(){
+    if (document.body.scrollTop > 65 || document.documentElement.scrollTop > 65){
+        document.getElementById("verticalnav").className = "visible";
+    } else {
+        document.getElementById("verticalnav").className = "";
+    }
+}
+
+
+// A bit of code I found on stackoverflow that makes buttons unfocus after you press them. Handy!
+document.addEventListener('click', function(e) { if(document.activeElement.toString() == '[object HTMLButtonElement]'){ document.activeElement.blur(); } });
